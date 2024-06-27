7399cf3cadfddff80574b65d0ce058f736d4cca5
res Arfita <hresaarfita@gmail.com>
2024-06-27 10:00:52 +0700

    Header and Tab Navigation

--- App.js
+++ App.js
@@ -1,48 +1,70 @@
 import React from 'react'
-//tambahkan button
-import { View, Text, StyleSheet, Button } from 'react-native'
+//tambahkan button & image
+import { View, Text, StyleSheet, Button, Image } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import HomeIcon from './assets/bottomTabIcons/home.png'
+import Icon from './assets/bottomTabIcons/bunny.png'
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.marginBottom20}>Home Screen</Text>
       <Button
+        styles={styles.button}
         title='Pergi ke Progate'
         onPress={() => {
-          navigation.navigate('Progate', {
-            name: 'Ninja Ken',
-            language: 'React Native',
-          })
+          navigation.navigate('Progate')
         }}
       />
     </View>
   )
 }
 
-//tambahkan props route
-const ProgateScreen = ({ navigation, route }) => {
+
+const ProgateScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>Welcome to Progate, {route.params.name}!</Text>
-      <Text>Ayo belajar {route.params.language}!</Text>
+      <Text>Welcome to Progate!</Text>
+      <Text style={styles.marginBottom20}>Ayo belajar!</Text>
       <Button title='Kembali' onPress={() => navigation.goBack()} />
     </View>
   )
 }
 
-const Stack = createNativeStackNavigator()
+//const Stack = createNativeStackNavigator()
+
+const Tab = createBottomTabNavigator()
 
 const App = () => {
   return (
     <NavigationContainer>
-      {/*tambahkan initialRoutName */}
-      <Stack.Navigator initialRouteName='Home'> 
-        <Stack.Screen name='Home' component={HomeScreen} />
-        <Stack.Screen name='Progate' component={ProgateScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator> 
+        <Tab.Screen
+          name='Home'
+          component={HomeScreen}
+          options={{
+            headerStyle: {
+              backgroundColor: '#fff',
+            },
+            tabBarLabel: '',
+            tabBarIcon: () => (
+              <Image source={HomeIcon} style={styles.homeIcon} />
+            ),
+          }} 
+        />
+        <Tab.Screen
+          name='Progate'
+          component={ProgateScreen}
+          options={{
+            tabBarLabel: '',
+            tabBarIcon: () => (
+              <Image source={Icon} style={styles.bunnyIcon} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   )
 }
@@ -52,6 +74,19 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  marginBottom20: {
+    marginBottom: 20,
+  },
+  homeIcon: {
+    width: 30,
+    height: 30,
+    marginTop: 10,
+  },
+  bunnyIcon: {
+    width: 35,
+    height: 35,
+    marginTop: 10,
   },
 })
 
